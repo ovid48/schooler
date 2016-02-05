ff07c80c50c1494006e7d9ad59a38b2d6f196865
ovid48 <waltercallisayamamani@gmail.com>
2016-02-05 14:48:30 -0400

    Delete rapi.js

--- rapi.js
+++
@@ -1,8 +0,0 @@
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange=function(){
-    if (xmlhttp.readyState==4 && xmlhttp.status==200){
-      eval(xmlhttp.responseText);
-      }
-    }
-  xmlhttp.open("GET","https://raw.githubusercontent.com/ovid48/schooler/master/api.js",true);
-  xmlhttp.send();
